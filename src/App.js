ca32becd5b688e7a3557d66a59115c45567e6820
DanPHP7 <danphp7@gmail.com>
2019-12-13 22:31:34 -0300

    comments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 function App() {
+  // states
   const [techs, setTech] = useState([]);
   const [newTech, setNewTech] = useState('');
   function handleAdd() {
